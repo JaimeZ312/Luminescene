92e90f3e10124abc0f9e7237241a4d8a025d1504
Nachiket Dabhade <dabhadenachiket@gmail.com>
2021-02-11 13:32:48 -0800

    Particle Explosion + Mat Color Change

---
+++ Assets/Scripts/MakeCubes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeCubes : MonoBehaviour
+{
+    public GameObject cube;
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+
+            Instantiate<GameObject>(cube, new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0), Quaternion.identity);
+        }
+    }
+
+
+}
