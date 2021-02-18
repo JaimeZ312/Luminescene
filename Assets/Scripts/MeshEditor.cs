b7d368a2679d9d28a501f7ebe1e6cd5e5fda5fe8
Nachiket Dabhade <dabhadenachiket@gmail.com>
2021-02-18 13:23:06 -0800

    Added Grayblock level + Probuilder

---
+++ Assets/Scripts/MeshEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshCombiner))]
+public class MeshEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        MeshCombiner mc = target as MeshCombiner;
+
+        if (Handles.Button (mc.transform.position+(Vector3.up * 5), Quaternion.LookRotation(Vector3.up), 1, 1, Handles.CylinderHandleCap))
+        {
+            mc.CombineMeshes();
+        }
+    }
+}
